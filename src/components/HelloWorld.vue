fd6b9d4dd4c0ea7857a430dd15e40ce2640e5396
VivianZenika <133650407+VivianZenika@users.noreply.github.com>
2023-09-27 13:34:00 +0200

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useCounterStore } from '../stores/counter';
+const store = useCounterStore();
+</script>
+
+<template>
+  From HelloWorld:
+  <div class="card">
+    {{ store.doubleCount }}
+    <button type="button" @click="store.increment">
+      count is {{ store.count }}
+    </button>
+  </div>
+</template>
